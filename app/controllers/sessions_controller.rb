8f463edca5c7a9d8417b3bb04d6513e6d05fb400
Jim Brazeal <jim.brazeal@kellogg.northwestern.edu>
2023-05-18 01:57:52 +0000

    Synch 2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,14 +10,13 @@ class SessionsController < ApplicationController
 
     # 2. if the user exists -> check if they know their password
     if @user != nil
-
-    # 3. if they know their password -> login is successful
-      if @user["password"] == params["password"]
-    
-      # 4. if the email does not exist or they do not know their password -> login fails
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        # 3. if they know their password -> login is successful
+        session["user_id"] = @user["id"]
         flash["notice"] = "Welcome."
         redirect_to "/companies"
       else
+        # 4. if the email does not exist or they do not know their password -> login fails
           flash["notice"] = "Nope."
           redirect_to "/sessions/new"
       end
@@ -29,6 +28,7 @@ class SessionsController < ApplicationController
 
   def destroy
     # logout the user
+    session["user_id"] = nil
     flash["notice"] = "Goodbye."
     redirect_to "/sessions/new"
   end
